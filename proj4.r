7ed61acd859c824677e7f5d18a90cde6c2ee0356
m.a.a.a@sms.ed.ac.uk <435108315@student.ksu.edu.sa>
2022-11-15 16:48:47 +0000

    push the code

--- proj4.r
+++ proj4.r
@@ -15,10 +15,49 @@ grad<- function(func, paras){
 }
   
 
+#if hessian matrix not provided, an approximation to Hessian is provided by finite differencing approximation
+#of the the gradient vector, finding the hessian matrix
+if (hess ==0) {
+  He <- function (theta, t,y) {
+    ##hessian of the function  
+    alpha
+    
+  }}
+
+#test the hessian by finite difference aprox
+hees <- grad(theta,t, y) ##grad of grad at
+eps <- le-8 ##fininte difference interval 
+Hfd <- matrix (0,)  #finite diference Hessian
+for (i in 1:length((theta))) {
+  the1 <- theta
+  the1[i] <- the1[i] + eps   ##compute resulting 
+  hess1 <- grad (the1,t= , y=y) ##compute resulting 
+  Hfd [i,] <- (hess1-hees)/eps  ##approximate second derives
+}
 
 newt<-function(theta,func,grad,hess=NULL,...,tol=1e-8, fscale=1,maxit=100, max.half=20, eps=1e-6){
   
  
+  x= theta
+  
+  
+  
+  #while loop to find the min value of the objective fucntion
+    #counter to count the iteration
+   counter=1
+  
+   #while loop to find the min value for the obj function
+  while (abs(func(x,...))>tol || counter <= maxit)  {
+    
+    
+    
+  
+    
+    
+    counter <- counter+1 #add number of iteration
+  }
+  
+  
   
   
   rl<- list(f,theta,iter,g,Hi)
