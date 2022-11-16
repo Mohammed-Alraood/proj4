7eed4fc6ac75bdb68b838b159892df3691a50508
m.a.a.a@sms.ed.ac.uk <435108315@student.ksu.edu.sa>
2022-11-16 17:48:56 +0000

    change the loop

--- proj4.r
+++ proj4.r
@@ -45,21 +45,45 @@ newt<-function(theta,func,grad,hess=NULL,...,tol=1e-8, fscale=1,maxit=100, max.h
   #while loop to find the min value of the objective fucntion
     #counter to count the iteration
    counter=1
-  
    #while loop to find the min value for the obj function
-  while (abs(func(x,...))>tol || counter <= maxit)  {
+  while (abs(rb(x))>tol || counter <= maxit)  {
+    x= x-rb(x)/gb(x)
     
+  }
+   x
     
     
   
     
     
     counter <- counter+1 #add number of iteration
+    
+    rl<- list(f,theta,iter,g,Hi)
+    return(rl)
   }
   
   
   
   
-  rl<- list(f,theta,iter,g,Hi)
-  return(rl)
+  
+
+
+
+
+
+#given functions
+
+rb <- function(th,k=2) {
+  k*(th[2]-th[1]^2)^2 + (1-th[1])^2
+}
+gb <- function(th,k=2) {
+  c(-2*(1-th[1])-k*4*th[1]*(th[2]-th[1]^2),k*2*(th[2]-th[1]^2))
+}
+
+hb <- function(th,k=2) {
+  h <- matrix(0,2,2)
+  h[1,1] <- 2-k*2*(2*(th[2]-th[1]^2) - 4*th[1]^2)
+  h[2,2] <- 2*k
+  h[1,2] <- h[2,1] <- -4*k*th[1]
+  h
 }
