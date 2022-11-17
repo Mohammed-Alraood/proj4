ad5c3a3b4337724c31ea49e9f10ffbf090634388
Chloe Bircham <chloebircham@sky.com>
2022-11-17 20:19:42 +0000

    detailed comments

--- proj4.r
+++ proj4.r
@@ -1,3 +1,11 @@
+#Group #36
+#Group Member: Chloe Bircham s2415006, Mohammed Alraood s2227255, Xinyan Chen s2304087
+#https://github.com/Mohammed-Alraood/proj4.git
+#contribution: 
+#Chloe Bircham:
+#Mohammed Alraood:
+#Xinyan Chen:
+
 fdhess<-function(theta,grad,hess){
   #function for finding finite difference of hessian if it is not provided
   #input theta values, gradient vector and hess provided or =NULL
@@ -16,28 +24,55 @@ if (is.null(hess)==TRUE) {
     nh<-Hfd
   }
 }else{
+  #if hess was provided, evaluate it at theta
   nh<-hess(theta)
 }
   return(nh)
+  #return the hessian
 }
 
 
 newt<-function(theta,func,grad,hess=NULL,...,tol=1e-8, fscale=1,maxit=100, max.half=20, eps=1e-6){
-
+#newt function that optimized a given objective function buy applying the newton method.
+#INPUT:
+  #theta: vector of inital values
+  #func: objective function to minimize
+  #grad: the gradient function for the objective function
+  #hess: the hessian matrix function for the objective function. if equal to null, newt computes
+    #hess by finite differencing
+  #the tolerance set for convergence
+  #fscale: estimate for magnitude of function near the optimum
+  #maxit: limit for number of iterations
+  #max.half: max number of halving steps to increase objective function
+  #eps: finite difference interval for calculating hessian if not provided
+  
+#OUTPUT: list of
+  #f: objective function at minimum value
+  #theta: value of parameters at the minimum
+  #iter: number of iterations needed to reach minimum
+  #g: gradient vector at the minimum
+  #Hi: the inverse of the hessian at minimum
+  #
+  
+  
+  
+  
   #call finite difference hessian function to determine hessian matrix, if hess defined or not
   nh<-fdhess(theta,grad,hess)
   
-  #evaluate function and grad at theta
+  #evaluate function and grad at initial theta
   nf<-func(theta)
   ng<-grad(theta)
 
-  #find eigenvalues for postive definitiveness 
+  #find eigenvalues for evaluating if postive definitive 
   eigenvals<-eigen(nh)[[1]]
 
   while(any(eigenvals<=0)==TRUE){
     #if eigenvalues are negative, matrix is not positive definite
     #perturb hessian to arrive at a positive definite matrix
+    #perturb by addition of a multiple of the identity matrix
     nh<- nh + length(theta)*diag(length(theta))
+    #reassign eigenvals to test if positive definite
     eigenvals<-eigen(nh)[[1]]
   }
   
@@ -49,36 +84,53 @@ newt<-function(theta,func,grad,hess=NULL,...,tol=1e-8, fscale=1,maxit=100, max.h
   }
   
   
-  #check if we have reached convergence
+  #check if we have reached convergence, using parameters set by the function
   check<-abs(ng) <  tol*abs(nf) + fscale
+  
+  #set a counter=0 to count iterations
   count=0
+  
+  #checking if the absolute value of the function at the current value is less than consitions for convergence
+    ##if it is not then:
   while(any(check==FALSE) ){
-    
     #Check if maximum iterations have been reached, if they have without convergence, exit method
     if(count > maxit){
       stop("max iterations reached: did not reach convergence")
     }
     
+    #add one to nuber of iterations
     count=count+1
+    
     #minimizing function
+    #negative inverse of the hessian multiplied with the gradient (both evaluated at current value)
     mf<- -chol2inv(chol(nh)) %*% ng
 
+    #assigning a new value of theta as the current value plus the minimizing function
      trynew<- theta+mf
-count2<-0
+     #set another counter =0 to count number of halving steps performed
+    count2<-0
+    
+    #check if the function evaluated at the new values have inproved the objective function
+    #if they havent, then:
     while(nf< func(trynew)){
+      
+      #check we havent reached maximum halving steps, if we have, stop iterations, print error message
       if(count2>max.half){
         stop("Reaches maximum limit of step halving: did not reach convergence")
       }
+      
+      #half the minimizing function and add these to the theta values. reiterate the loop to 
+      #check if these values minimize the function. Loop ends when appropriate values are found
       mf<-0.5*mf
       trynew<- theta+mf
     
 
     }
 
-
+#Assign the new values of theta as the previous steps values + new minimixing function
     theta<-theta+mf
     
-    
+    #evaluate function, gradient and hessian at the new theta
     nf<-func(theta)
     ng<-grad(theta)
   #computing new hessian at min
@@ -89,6 +141,7 @@ count2<-0
     check<-abs(ng) <  tol*nf + fscale
     
   }
+  
   #function evaluated at the minimum
   fmin<- nf
   #iterations taken to find minimum
